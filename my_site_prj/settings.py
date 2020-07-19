713018cc3010bba763276429a4d7eaab9aa1b7ed
whitecastle20 <sjtjdwn20@gmail.com>
2020-07-20 08:54:26 +0900

    pretty google login

--- my_site_prj/settings.py
+++ my_site_prj/settings.py
@@ -39,6 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    'allauth.socialaccount.providers.google',
+
     'markdownx',
     'crispy_forms',
 ]
@@ -116,6 +125,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 
 # Static files (CSS, JavaScript, Images)
--- my_site_prj/urls.py
+++ my_site_prj/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
